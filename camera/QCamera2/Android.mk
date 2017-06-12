1a64e9f0bd693f362cc53981c3a0cf9bdf3171c1
Jiyong Park <jiyong@google.com>
2017-06-12 17:58:31 +0900

    mido: camera: Replace libcamera_client to android.hardware.camera.common@1.0-helper
    libcamera_client is not a VNDK library and thus not available to
    vendors. These vendor libs were using the library due to CameraMetadata
    and CameraParamters classes. The classes are now available via
    .common@1.0-helper library which is available to vendors. Thus switching
    to the lib.
    
    Bug: 62523241
    Test: 2016 pixel devices builds and boots. Camera functionalities work.
    Change-Id: Idc25ce56ee5175aea4243fbce770f240ff0b62ca

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -104,6 +104,7 @@ ifneq (,$(filter msm8996 msmcobalt msmfalcon,$(TARGET_BOARD_PLATFORM)))
 endif
 
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
+LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
