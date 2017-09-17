d84eecad12081e80f9fff37c0a0d7c1bffd24cd5
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    mido: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- device.mk
+++ device.mk
@@ -359,7 +359,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.basic_mido
 
 # Vibrator
 PRODUCT_PACKAGES += \
