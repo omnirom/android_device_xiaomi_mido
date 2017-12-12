2ed3161db8b450b88b487ec0187c9d438914056d
khanfrd <khanfrd94@gmail.com>
2017-12-12 05:51:51 +0000

    mido: fix init build
    Change-Id: Ib31147d62df84363d2f5f798383ca8263e796e76

--- init/init_mido.cpp
+++ init/init_mido.cpp
@@ -28,11 +28,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/sysinfo.h>
+#include <android-base/logging.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
+
+namespace android {
+namespace init {
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
@@ -129,3 +132,5 @@ void vendor_load_properties()
     property_set("ro.hwui.text_large_cache_width", "2048");
     property_set("ro.hwui.text_large_cache_height", large_cache_height);
 }
+}  // namespace init
+}  // namespace android
