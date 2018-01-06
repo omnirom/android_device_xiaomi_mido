9fb38f244b4c32c7d3065c277c5d6519addeeb40
khanfrd <khanfrd94@gmail.com>
2018-01-06 13:37:22 +0000

    Revert "mido: use dalvik/hwui config"
    This reverts commit 6504d691826ccaff4b955c311275f419bdd26e58.
    
    Change-Id: Ied69ba3bf368ae058c064212aa111bbba12a2a85

--- device.mk
+++ device.mk
@@ -17,8 +17,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
-$(call inherit-product, vendor/omni/config/phone-xxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product, vendor/omni/config/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
