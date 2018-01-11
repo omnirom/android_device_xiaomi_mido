950e1c4952dd3da436ea21ca7a98279c0d799086
khanfrd <khanfrd94@gmail.com>
2018-01-11 22:10:07 +0000

    mido: build hidl manager 1.0-java too
     * failed: Package com.qualcomm.qcrilmsgtunnel requires unavailable shared library android.hidl.manager@1.0-java; failing!
    
    Change-Id: I8c0933309ba5f59d0dfdc8a6c93a559427776120

--- device.mk
+++ device.mk
@@ -212,7 +212,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # Input
 PRODUCT_COPY_FILES += \
