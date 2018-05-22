046dca2f2918f8a1ca6ce0ac5000f858591c5c68
TheScarastic <warabhishek@gmail.com>
2018-05-22 14:31:28 +0530

    mido: Generate thermal-engine.conf
     * adb shell thermal-engine –o > thermal-engine.conf
    
    Change-Id: If934f33d43c7d74008e63ceef8dd03d420c624dc

--- device.mk
+++ device.mk
@@ -250,6 +250,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # VNDK-SP:
 PRODUCT_PACKAGES += \
     vndk-sp
