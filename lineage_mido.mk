d16e6549f04889ebf76ffb6eb21ff56c43c7c515
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-07-06 22:34:26 -0300

    mido: Include MIUI Camera if the makefile exists
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit from custom vendor
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := lineage_mido
