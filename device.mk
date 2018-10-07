4b64752f1ff8ba49dd2186186e0cbea0f7fb9bab
sayan7848 <sayan7848@gmail.com>
2018-10-07 17:28:52 +0000

    Reinclude power qti service
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- device.mk
+++ device.mk
@@ -283,6 +283,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-service-qti \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.0-service
 
