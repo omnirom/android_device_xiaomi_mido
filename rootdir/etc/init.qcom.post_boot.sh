6d5f0e4073ab292ee0069690d12832c271c6d28d
Razziell <razziell@ya.ru>
2018-11-21 18:03:11 +0300

    init.qcom.post_boot: Virtual memory tweaks

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1800,6 +1800,10 @@ case "$target" in
                 echo 75 > /proc/sys/kernel/sched_upmigrate
                 echo 60 > /proc/sys/kernel/sched_downmigrate
 
+                # Virtual memory tweaks
+                echo 10 > /proc/sys/vm/swappiness
+                echo 10 > /proc/sys/vm/dirty_background_ratio
+
                 # Set Memory parameters
                 configure_memory_parameters
             ;;
