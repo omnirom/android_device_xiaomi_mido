6b2339122006956e145c8748dd7b8df66e2ba369
davidtrpcevski <trpcevski.david@gmail.com>
2018-12-31 07:09:01 +0100

    mido: Set the first API level the device shipped on
    First launched with MIUI 8.1 based Android 6.0, upgraded to Android 7.0
    
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -205,6 +205,9 @@ TARGET_USE_SDCLANG := true
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 23
+
 # RIL
 TARGET_RIL_VARIANT := caf
 
