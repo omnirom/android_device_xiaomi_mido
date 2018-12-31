40192230e940992d44f9fc63d434a7b2e5da4d67
Silesh K Nair <sileshn@gmail.com>
2018-12-31 06:04:40 +0530

    Add Aptx blobs from Nokia/Onyx_00WW/B2N_sprout:9/PPR1.180610.011/00WW_3_39B

--- prop.mk
+++ prop.mk
@@ -13,6 +13,7 @@ persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=true \
 persist.vendor.audio.hw.binder.size_kbyte=1024 \
 persist.vendor.btstack.enable.splita2dp=false \
+persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
 ro.af.client_heap_size_kbyte=7168 \
 ro.vendor.audio.sdk.fluencetype=fluence \
 ro.vendor.audio.sdk.ssr=false \
