749b3fd307b3e8bcd805c2ebdd4300154b787968
davidtrpcevski <trpcevski.david@gmail.com>
2019-01-17 10:49:12 +0100

    Revert "mido: Update heap sizes"
    This reverts commit 18ed4f84593817d03c15a8905af257b585f2d5b2.
    
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- init/init_msm8953.cpp
+++ init/init_msm8953.cpp
@@ -33,9 +33,7 @@
 #include "property_service.h"
 #include "log.h"
 
-char const *heapstartsize;
 char const *heapgrowthlimit;
-char const *heapsize;
 char const *heapminfree;
 
 using android::init::property_set;
@@ -46,24 +44,13 @@ void check_device()
 
     sysinfo(&sys);
 
-    if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "16m";
+    if (sys.totalram > 2048ull * 1024 * 1024) {
+        // from - Stock rom
         heapgrowthlimit = "256m";
-        heapsize = "512m";
         heapminfree = "4m";
-    } else if (sys.totalram > 2048ull * 1024 * 1024) {
-        // from - phone-xxhdpi-3072-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "288m";
-        heapsize = "768m";
-        heapminfree = "512k";
-        
     } else {
         // from - phone-xxhdpi-2048-dalvik-heap.mk
-        heapstartsize = "16m";
         heapgrowthlimit = "192m";
-        heapsize = "512m";
         heapminfree = "2m";
    }
 }
@@ -72,9 +59,9 @@ void vendor_load_properties()
 {
     check_device();
 
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
+    property_set("dalvik.vm.heapstartsize", "16m");
     property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
+    property_set("dalvik.vm.heapsize", "512m");
     property_set("dalvik.vm.heaptargetutilization", "0.75");
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", "8m");
