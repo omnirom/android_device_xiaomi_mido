f4b0cdaade087a7d0bce2afa801e39b194677252
sayan7848 <sayan7848@gmail.com>
2019-01-24 19:19:21 +0000

    Ignore sepolicy neverallow for now
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,6 +225,7 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_VERS := 28.0
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
