9686b781a5ebe21178e8b097c50a508daa37048d
ezio84 <brabus84@gmail.com>
2019-01-24 22:16:02 +0100

    mido: doze: Allow to change default Ambient brightness values
    needs this:
    https://github.com/ezio84/abc_vendor_nexus/commit/29faa1f3092f1ffa20aecbb7a7a9f4c189cae71f
    
    and a device overlay for hasDozeBrightnessSensor
    and config_doze_brightness_sensor_to_brightness
    
    TODO: understand how AoD brightness gets calculated and improve this
    feature, atm i've no free time to spend on this :P
    
    - SystemUI AlwaysOnDisplayPolicy settings observer already takes care
    to update its screenBrightnessArray public int when Settings.Global.ALWAYS_ON_DISPLAY_CONSTANTS)
    is changed
    
    Change-Id: Id92463783b62c75746132f6daf1dad0888ead8c8

--- doze/Android.mk
+++ doze/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_USE_AAPT2 := true
 
 LOCAL_PACKAGE_NAME := XiaomiDoze
 LOCAL_PRIVATE_PLATFORM_APIS := true
@@ -12,6 +13,8 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
 
