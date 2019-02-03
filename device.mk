1aa561ee8726c7456440d13390d46ee3ea6a4a39
RinKirito <abdul.4pda05@yandex.com>
2019-02-03 09:03:04 +0000

    include dalvik-heap config
    Change-Id: I6fec7d83711ee55ea81fe79d54b50ecb30bf4070

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, vendor/omni/config/phone-xxhdpi-3072-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
