84822d467114e2f0cf354dcf207c265fb13f5570
HarukeyUA <inazarrusnak@gmail.com>
2019-03-03 10:19:54 +0000

    mido: improve bt+wifi coexistence
    Thanks to silent27121984 for fix

--- prop.mk
+++ prop.mk
@@ -47,10 +47,11 @@ vendor.voice.voip.conc.disabled=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.bt.enable.splita2dp=false \
 bluetooth.hfp.client=1 \
 vendor.qcom.bluetooth.soc=smd \
 ro.bluetooth.hfp.ver=1.7 \
-ro.qualcomm.bt.hci_transport=smd
+ro.vendor.qualcomm.bt.hci_transport=smd
 
 # Boot
 PRODUCT_PROPERTY_OVERRIDES += \
