eb7a8561e5d4d3f2f349ad4c98c91844eb93be5d
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2019-03-18 21:24:33 +0200

    mido: Initial FlipFlap support

--- device.mk
+++ device.mk
@@ -188,6 +188,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido
 
+# FlipFlap
+PRODUCT_PACKAGES += \
+    FlipFlap
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
