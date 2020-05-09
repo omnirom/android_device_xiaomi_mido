40ce060460f87307d6df7b2a1b5c88be8549866a
Ali Hasan <ahb7671@gmail.com>
2020-05-09 14:53:29 +0500

    mido: DeviceParts: Add display calibration
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>
    Change-Id: Ifb689085220f5cc949dd546be5e39672d72186e4

--- prop.mk
+++ prop.mk
@@ -82,7 +82,6 @@ ro.opengles.version=196610 \
 ro.qualcomm.cabl=0 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.gralloc.enable_fb_ubwc=1
 
