ab79e917cbec3683ce07abf5144e5a9e28a19401
Ali Hasan <ahb7671@gmail.com>
2020-12-17 14:45:52 +0500

    mido: import power hal
    from lineage
    https://github.com/LineageOS/android_vendor_qcom_opensource_power
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

---
+++ power/Android.mk
@@ -0,0 +1,49 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libbase \
+    libutils \
+    android.hardware.power-ndk_platform \
+    libbinder_ndk
+
+LOCAL_HEADER_LIBRARIES := \
+    libhardware_headers
+
+LOCAL_SRC_FILES := \
+    power-common.c \
+    metadata-parser.c \
+    utils.c \
+    list.c \
+    hint-data.c \
+    Power.cpp \
+    main.cpp \
+    power-8953.c
+
+LOCAL_CFLAGS += -Wall -Wextra -Werror
+
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+endif
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+LOCAL_MODULE := android.hardware.power-service-qti
+LOCAL_INIT_RC := android.hardware.power-service-qti.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
+LOCAL_VENDOR_MODULE := true
+LOCAL_VINTF_FRAGMENTS := power.xml
+
+include $(BUILD_EXECUTABLE)
+endif
