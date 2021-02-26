278e20ce778bc9500eeceed3e337c23acd5c1a7d
Ali Hasan <ahb7671@gmail.com>
2021-02-26 10:12:31 +0500

    mido: call aosp telephony base
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>
    Change-Id: Ice21141467acce1ce509776d7186cb8340ff1054

--- omni_mido.mk
+++ omni_mido.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
